7f343ccadd8c5c6f54e6f9b1347c4641895bc828
vishal shankhdhar <vishalshankhdhar72@gmail.com>
2026-09-01 23:01:28 +0530

    Fix backend deployment configuration

--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,7 @@ import cors from "cors";
 import authRouter from "./Routes/auth.route.js";
 import userRouter from "./Routes/user.route.js";
 import interviewRouter from "./Routes/interview.route.js";
+// import path from "path";
 
 //connectivity between FRONTEND AND BACKEND HERE----
 
@@ -21,6 +22,8 @@ app.use(
     credentials: true,
   }),
 );
+
+// const _dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,9 +37,13 @@ app.get("/", (req, res) => {
   res.send("AI InterviewIQ Backend is Working!");
 });
 
-
-
 const PORT = process.env.PORT || 6000;
+
+// app.use(express.static(path.join(_dirname, "/Client/dist")));
+
+// app.get("*", (_, res) => {
+//   res.sendFile(path.resolve(_dirname, "Client", "dist", "index.js"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT ${PORT}`);
