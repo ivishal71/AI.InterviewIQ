d8371c717859aefe8af431bb647ba48295713243
vishal shankhdhar <vishalshankhdhar72@gmail.com>
2026-09-02 00:09:47 +0530

    Fix authentication cookies for production

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const googleAuth = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -36,7 +36,11 @@ export const googleAuth = async (req, res) => {
 //CONTROLLER FOR LOGOUT----
 export const logOut = async (req, res) => {
   try {
-    await res.clearCookie("token");
+    await res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
     return res.status(200).json({ message: "logOut Successfully" });
   } catch (error) {
     return res.status(500).json({ message: `Logout Error ${error}` });
